e338d485135eb2db5eea7d2aa87bb952155a5691
David Havera <djhavera@yahoo.com>
2019-09-06 20:42:50 -0400

    Update truffle-config.js
    Updated path so that src/truffle-config can pull abi... updated port for ganche gui

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,12 @@
+const path = require("path")
+
 module.exports = {
+  contracts_build_directory: path.join(__dirname, "src/configs/contracts"),
   migrations_directory: "./migrations",
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     }
   }
